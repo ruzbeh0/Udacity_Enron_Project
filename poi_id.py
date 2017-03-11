28d93b650789db62c4846ede13838e99ab1d8554
ruzbeh0 <ruzbeh0@gmail.com>
2017-03-11 10:11:45 -0500

    Commit

--- poi_id.py
+++ poi_id.py
@@ -10,10 +10,11 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary', 'to_messages', 'deferral_payments', 'total_payments', 'exercised_stock_options',
-                 'bonus', 'restricted_stock', 'shared_receipt_with_poi', 'restricted_stock_deferred',
-                 'total_stock_value', 'expenses', 'loan_advances', 'from_messages', 'other', 'from_this_person_to_poi',
-                 'director_fees', 'deferred_income', 'long_term_incentive', 'from_poi_to_this_person']
+features_list = ['poi','salary', 'to_messages', 'deferral_payments', 'total_payments',
+                 'exercised_stock_options', 'bonus', 'restricted_stock', 'shared_receipt_with_poi',
+                 'restricted_stock_deferred', 'total_stock_value', 'expenses', 'loan_advances',
+                 'from_messages', 'other', 'from_this_person_to_poi', 'director_fees', 'deferred_income',
+                 'long_term_incentive', 'from_poi_to_this_person']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -32,7 +33,7 @@ poi, features = targetFeatureSplit( data )
 model = RandomForestClassifier(random_state = 1)
 model.fit(features, poi)
 #Selecting features with importance higher than 8%
-#print "Features & Importances that are higher than 8%:"
+#select "Features & Importances that are higher than 8%:"
 i = 1
 new_features_list = ['poi']
 for importance in model.feature_importances_:
@@ -43,18 +44,26 @@ for importance in model.feature_importances_:
 
 features_list = new_features_list
 
+# Adding 'from_this_person_to_poi' and 'from_poi_to_this_person' to features_list to be used later
+features_list.append('from_this_person_to_poi')
+features_list.append('from_poi_to_this_person')
+
 data = featureFormat(data_dict, features_list )
 poi, features = targetFeatureSplit( data )
 total_payments = []
 exercised_stock_options = []
 bonus = []
 deferred_income = []
+from_this_to_poi = []
+from_poi_to_this = []
 
-for f1, f2, f3, f4 in features:
+for f1, f2, f3, f4, f5, f6 in features:
     total_payments.append(f1)
     exercised_stock_options.append(f2)
     bonus.append(f3)
     deferred_income.append(f4)
+    from_this_to_poi.append(f5)
+    from_poi_to_this.append(f6)
 
 ### Task 2: Remove outliers
 import matplotlib.pyplot as plt
@@ -71,34 +80,26 @@ pred = reg.predict(np.transpose(np.matrix(total_payments)))
 # Calculating the error
 pred_error = abs(pred - np.transpose(np.matrix(total_payments)))
 pred_error = pred_error.tolist()
-## Remove 7% of the records with maximum errors
-error_index_array = []
-for i in range(int(len(total_payments)*0.07)):
-    index = pred_error.index(max(pred_error))
-    error_index_array.append(index)
-    del pred_error[index]
+## Remove 2 outliers:
+## THE TRAVEL AGENCY IN THE PARK
+## TOTAL
+error_index_array = [101, 98]
+invalid_keys.append('THE TRAVEL AGENCY IN THE PARK')
+invalid_keys.append('TOTAL')
 
 #print error_index_array
-
 for i in error_index_array:
     del exercised_stock_options[i]
     del total_payments[i]
     del bonus[i]
     del deferred_income[i]
     del poi[i]
+    del from_this_to_poi[i]
+    del from_poi_to_this[i]
 
 for key in data_dict.keys():
     if key in invalid_keys:
         del data_dict[key]
-
-i = 0
-for key in data_dict.keys():
-    if i in error_index_array:
-        del data_dict[key]
-    i += 1
-
-#plt.scatter(total_payments, exercised_stock_options)
-#plt.show()
 
 data = []
 
@@ -107,17 +108,32 @@ for i in range(len(poi)):
 
 ### Task 3: Create new feature(s)
 
+# Will create a new feature based on communication with a POI
+# using existing features: 'from_this_person_to_poi' and 'from_poi_to_this_person'
+
+data_with_new_feature = []
+
+# The new feature indicates if there was communication with POI
+for i in range(len(poi)):
+    communication_with_poi = (from_this_to_poi[i] or from_poi_to_this[i])
+    data_with_new_feature.append(np.asarray([total_payments[i],exercised_stock_options[i],
+                                             bonus[i],deferred_income[i],communication_with_poi]))
+
 # Use a PCA to create three new features
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
 
+# PCA without scaled data
 pca = PCA(n_components=3, svd_solver='auto')
 new_data = pca.fit_transform(data, poi)
-#print(pca.explained_variance_ratio_)
+
+# Create PCA with scaled data
+pca = PCA(n_components=3, svd_solver='auto')
+new_data_scaled = pca.fit_transform(scale(data), poi)
 
 ##Add PCA features to data dictionary
 i = 0
 for key in data_dict.keys():
-
     data_dict[key]['PCA_1'] = new_data[i][0]
     data_dict[key]['PCA_2'] = new_data[i][1]
     data_dict[key]['PCA_3'] = new_data[i][2]
@@ -127,12 +143,65 @@ for key in data_dict.keys():
 
 my_dataset = data_dict
 
-#### Extract features and labels from dataset for local testing
-#data = featureFormat(my_dataset, features_list, sort_keys = True)
-#labels, features = targetFeatureSplit(data)
+# Method to test the algorithm - A Stratified Shuffle Split Cross Validation is used
+def test_classifier(clf, features, labels, add_estimators = 10):
+    from sklearn.model_selection import StratifiedShuffleSplit
 
-labels = poi
-features = new_data
+    sss = StratifiedShuffleSplit(n_splits=10, test_size=0.4, random_state=42)
+    sss.get_n_splits(features, labels)
+
+    true_negatives = 0
+    false_negatives = 0
+    true_positives = 0
+    false_positives = 0
+
+    for train_idx, test_idx in sss.split(features, labels):
+        features_train = []
+        features_test = []
+        labels_train = []
+        labels_test = []
+        for ii in train_idx:
+            features_train.append(features[ii])
+            labels_train.append(labels[ii])
+        for jj in test_idx:
+            features_test.append(features[jj])
+            labels_test.append(labels[jj])
+
+        ### fit the classifier using training set, and test on test set
+        clf.n_estimators += add_estimators
+        clf.fit(features_train, labels_train)
+        predictions = clf.predict(features_test)
+        for prediction, truth in zip(predictions, labels_test):
+            if prediction == 0 and truth == 0:
+                true_negatives += 1
+            elif prediction == 0 and truth == 1:
+                false_negatives += 1
+            elif prediction == 1 and truth == 0:
+                false_positives += 1
+            elif prediction == 1 and truth == 1:
+                true_positives += 1
+            else:
+                print "Warning: Found a predicted label not == 0 or 1."
+                print "All predictions should take value 0 or 1."
+                print "Evaluating performance for processed predictions:"
+                break
+    try:
+        total_predictions = true_negatives + false_negatives + false_positives + true_positives
+        accuracy = 1.0 * (true_positives + true_negatives) / total_predictions
+        precision = 1.0 * true_positives / (true_positives + false_positives)
+        recall = 1.0 * true_positives / (true_positives + false_negatives)
+        f1 = 2.0 * true_positives / (2 * true_positives + false_positives + false_negatives)
+        f2 = (1 + 2.0 * 2.0) * precision * recall / (4 * precision + recall)
+
+        print "Accuracy:"
+        print accuracy
+        print "Precision:"
+        print precision
+        print "Recall:"
+        print recall
+    except:
+        print "Got a divide by zero when trying out:", clf
+        print "Precision or recall may be undefined due to a lack of true positive predicitons."
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -142,74 +211,49 @@ features = new_data
 
 # A random forest classifier was chosen
 from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier(n_estimators=200, random_state=42, warm_start='true')
+clf = RandomForestClassifier(n_estimators=10, random_state=42, warm_start='true')
+print "Test 1 - Random Forest Classifier without PCA"
+test_classifier(clf,data,poi)
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+clf = RandomForestClassifier(n_estimators=10, random_state=42, warm_start='true')
+print "Test 2 - Random Forest Classifier without PCA + new feature"
+test_classifier(clf,data_with_new_feature,poi)
+
+clf = RandomForestClassifier(n_estimators=10, random_state=42, warm_start='true')
+print "Test 3 - Random Forest Classifier with PCA using Scaled Data"
+test_classifier(clf,new_data_scaled,poi)
+
+clf = RandomForestClassifier(n_estimators=10, random_state=42, warm_start='true')
+print "Test 4 - Random Forest Classifier with PCA without Scaled Data"
+test_classifier(clf,new_data,poi)
+
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-# A Stratified Shuffle Split Cross Validation is used
+#Using Grid Search to estimate the best n_estimators parameter
+#from sklearn.model_selection import GridSearchCV
+#parameters = {'min_samples_split': range(2,10), 'min_samples_leaf':range(1,10)}
+#grid_search = GridSearchCV(clf, param_grid=parameters)
+#grid_search.fit(new_data, poi)
+#print(grid_search.best_estimator_)
 
-from sklearn.model_selection import StratifiedShuffleSplit
+#Result from Grid Search:
+#RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+#            max_depth=None, max_features='auto', max_leaf_nodes=None,
+#            min_impurity_split=1e-07, min_samples_leaf=2,
+#            min_samples_split=9, min_weight_fraction_leaf=0.0,
+#            n_estimators=110, n_jobs=1, oob_score=False, random_state=42,
+#            verbose=0, warm_start='true')
 
-sss = StratifiedShuffleSplit(n_splits=10, test_size=0.4, random_state=42)
-sss.get_n_splits(features, labels)
-
-true_negatives = 0
-false_negatives = 0
-true_positives = 0
-false_positives = 0
-
-for train_idx, test_idx in sss.split(features, labels):
-    features_train = []
-    features_test = []
-    labels_train = []
-    labels_test = []
-    for ii in train_idx:
-        features_train.append(features[ii])
-        labels_train.append(labels[ii])
-    for jj in test_idx:
-        features_test.append(features[jj])
-        labels_test.append(labels[jj])
-
-    ### fit the classifier using training set, and test on test set
-    clf.n_estimators += 20
-    clf.fit(features_train, labels_train)
-    predictions = clf.predict(features_test)
-    for prediction, truth in zip(predictions, labels_test):
-        if prediction == 0 and truth == 0:
-            true_negatives += 1
-        elif prediction == 0 and truth == 1:
-            false_negatives += 1
-        elif prediction == 1 and truth == 0:
-            false_positives += 1
-        elif prediction == 1 and truth == 1:
-            true_positives += 1
-        else:
-            print "Warning: Found a predicted label not == 0 or 1."
-            print "All predictions should take value 0 or 1."
-            print "Evaluating performance for processed predictions:"
-            break
-try:
-    total_predictions = true_negatives + false_negatives + false_positives + true_positives
-    accuracy = 1.0 * (true_positives + true_negatives) / total_predictions
-    precision = 1.0 * true_positives / (true_positives + false_positives)
-    recall = 1.0 * true_positives / (true_positives + false_negatives)
-    f1 = 2.0 * true_positives / (2 * true_positives + false_positives + false_negatives)
-    f2 = (1 + 2.0 * 2.0) * precision * recall / (4 * precision + recall)
-
-    print "Accuracy:"
-    print accuracy
-    print "Precision:"
-    print precision
-    print "Recall:"
-    print recall
-except:
-    print "Got a divide by zero when trying out:", clf
-    print "Precision or recall may be undefined due to a lack of true positive predicitons."
+clf = RandomForestClassifier(n_estimators=200, random_state=42,
+                             warm_start='true', min_samples_split=2, min_samples_leaf=1)
+print "Test 5 - Final Random Forest Classifier"
+test_classifier(clf,new_data,poi)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
